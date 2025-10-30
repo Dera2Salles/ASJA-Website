d526f2a452b3533afdf7c539dfc0fd3556ef9d78
Dera2S <dera.ah.14@gmail.com>
2025-10-30 09:55:29 +0300

    fix : fix constant

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,5 +1,6 @@
 export class ApiSource {
   static PORT: number = 3000;
   static url: string = '__BACKEND_URL__';
-  static strapiUrl: string = '__BACKEND_URL__';
+  static strapiUrl: string = '__STRAPI_URL__';
+  static botUrl: string = '__BOT_URL';
 }
