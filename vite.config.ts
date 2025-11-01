63727dc978716731488ab4ae479f0733da92663d
Dera2S <dera.ah.14@gmail.com>
2025-11-01 09:19:31 +0300

    dockerImage : modify for docker building

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Charge les variables d'environnement pour le build
-  const env = loadEnv(mode, process.cwd(), '');
-
   return {
     server: {
       host: true, // Permet l'accès depuis l'extérieur du container
